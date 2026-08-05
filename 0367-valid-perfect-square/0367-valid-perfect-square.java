4f5d23885606f67d3a3e54a86881e5ddcf0225fe
prafulpatil1121 <prafulpatil1121@gmail.com>
2026-08-05 14:25:57 +0530

    Time: 2 ms (17.92%), Space: 41.8 MB (88.86%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+    long i = 1 ;
+    while(i * i <= num){
+        if(i * i == num){
+        return true;
+    }
+    i++;
+    }
+    return false;
+}
+}
